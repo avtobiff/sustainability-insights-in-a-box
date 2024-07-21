0e0515b7a5ee1d958d4e43a50620b16b940063cc
Per Andersson <perander@cisco.com>
2024-07-21 01:25:36 -0700

    Update YANG-Push collector
    * Need git to install ncclient dependency.
    
    * Add confluence_kafka and cryptography to requirements.txt.
    
    * Make yang-push collector publish the poweff data.

--- yang-push_collector/yang-push_collector.py
+++ yang-push_collector/yang-push_collector.py
@@ -1,7 +1,12 @@
 from ncclient.operations.subscribe import *
 from ncclient import manager
 
+from utils import messaging
+from utils.applog import logconfig, logger
+
+
 device_params = {"name": "dev0"}
+
 
 def do_yang_push(host, port, username, password):
     with manager.connect(host=host, port=port, username=username,
@@ -10,8 +15,28 @@ def do_yang_push(host, port, username, password):
                                  "/poweff:poweff/stats/device-current-total-power-draw",
                                  "500")
         while True:
+            payload = messaging.consume(schedules_topic, "collector")
+            task_config = json.loads(payload)
+            customer = task_config["customer"]["name"]
+            sites = task_config["sites"]
+            device = task_config["device"]["name"]
+            conn_type = task_config["device"]["connection"]
+            loglevel = task_config["loglevel"]["console"]
+
+            logconfig(customer, loglevel)
+
+            # The YANG-Push notification
             notif = m.take_notification().notification_ele
-            print(notif[1][1][0][0][0].text)
+            device_current_total_power_draw = notif[1][1][0][0][0].text
+
+            logger.info(f"{device} sending poweff"
+                        f"{device_current_total_power_draw} to message queue")
+
+            batch_task.copy()
+            batch_task["poweff"] = device_current_total_power_draw
+            messaging.produce(topic="collections", key=device,
+                              message=batch_task)
+
 
 if __name__ == '__main__':
-    do_yang_push("localhost", 12022, username="admin", password="admin")
+    do_yang_push("172.19.0.1", 12022, username="admin", password="admin")
